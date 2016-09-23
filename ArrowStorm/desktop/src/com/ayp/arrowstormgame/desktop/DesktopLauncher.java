8cfe80eef4079c3aa93ec4480593b7fcb69aa89d
theerawuth <theerawuth.p@augmentis.biz>
2016-09-23 15:58:33 +0700

    Initialize Project

---
+++ ArrowStorm/desktop/src/com/ayp/arrowstormgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ayp.arrowstormgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ayp.arrowstormgame.ArrowStormGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ArrowStormGame(), config);
+	}
+}
